84bca3608ca446d356bb6ab4f2be2d51065b6123
Shane Sniff <slsniff@gmail.com>
2016-05-28 23:01:38 -0700

    New method in packetparse.py for full breakdown of pcap data.

--- packetparse.py
+++ packetparse.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python
 
+import csv
 import pyshark
+
+
+def breakdown_to_csv(pcapfile):
+    cap = pyshark.FileCapture(pcapfile)
+    fwrite = csv.writer(open('full_breakdown.csv', 'wb'))
+    fwrite.writerow(['frame_number', 'frame_time', 'eth_src', 'eth_dsst',
+                     'ip_src', 'ip_dst', 'ip_proto', 'frame_len'])
+    for i in cap:
+        fwrite.writerow([i.number, i.sniff_time.isoformat(' '), i.eth.src,
+                         i.eth.dst, i.ip.src, i.ip.dst, i.ip.proto, i.length])
+
 
 def bps(pcapfile):
     cap = pyshark.FileCapture(pcapfile)
